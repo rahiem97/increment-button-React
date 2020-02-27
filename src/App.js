fc0ee51523b4cc624ad6bc56795e3e54d4917a69
Rahiem Thompson <rahiemthompson@Rahiems-MacBook-Pro.local>
2020-02-27 13:43:20 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+//Functional component APP (Name is arbitrary but common)
 function App() {
+  
+  //This function uses hooks to save the counter data and update it on a button onClick listener
+  const FunctionalCounter = () => {
+    const [count, setCount] = useState(0);
+    //FunctionalCounter will return what I want this specific component to render
+    return (
+      <div>
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>
+          Click me
+        </button>
+      </div>
+    );
+  }
+  
+//The entire App Functional will return the full page layout (This is completely by design)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="MyRTWhite.png" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is my button incrementing React.js application. 
+  
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {FunctionalCounter()}
       </header>
     </div>
   );
-}
+} 
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,35 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//This is the class way of creating the button with the increment counter
+ class HelloWorld extends React.Component {
+     constructor() {
+       super();
+       this.state = {counter: 0};
+     }
+     incrementCounter = () => {
+       return this.setState({counter: this.state.counter + 1})
+     }
+     
+     render() {
+       const {
+         counter
+       } = this.state
+       return (
+         <div>
+           Counter: {counter}
+           <br/>
+           <button onClick={this.incrementCounter}>Increment</button>
+           <hr />
+        
+         </div>
+       )
+     }
+   }
+
+  
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
